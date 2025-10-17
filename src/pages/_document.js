a1dd3ecf660e3da8ba213c49a0f881a4d4095b0a
Fauzan <arroyyanfauzan4@gmail.com>
2025-10-17 13:39:09 +0700

    ubah dokum

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="id">
       <Head>
-        {/* ✅ Meta viewport fix untuk zooming */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-
-        {/* Meta tags dan font optional */}
+        {/* FIX: Viewport meta wajib untuk Vercel SSR */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        {/* Google Font */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Space+Mono:wght@400;700&display=swap"
           rel="stylesheet"
         />
       </Head>
@@ -20,5 +22,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
