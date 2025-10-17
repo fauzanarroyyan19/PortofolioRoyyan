59b919583d663a2b2b33a3faa77c0dd2d88c27d6
Fauzan <arroyyanfauzan4@gmail.com>
2025-10-17 13:31:40 +0700

    fix: add viewport meta to fix zoom issue on deploy

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="id">
       <Head>
+        {/* ✅ Meta viewport fix untuk zooming */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
         {/* Meta tags dan font optional */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
